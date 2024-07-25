6c2b2491d911b888eff55414c1d4a5a7c8570593
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-07-25 22:22:14 +0530

    array forEach

--- YoutubeBackend/script.js
+++ YoutubeBackend/script.js
@@ -2,3 +2,18 @@
 // Arrays and objects
 // functions return
 // async js coding
+
+// ARRAYS
+// forEach map filter find indexOf
+var arr = [1,2,3,4]
+// forEach
+// arr.forEach(function(val) {
+//     console.log(val + " hello");
+// })
+// OUTPUT:
+// 1 hello
+// 2 hello
+// 3 hello
+// 4 hello
+
+
