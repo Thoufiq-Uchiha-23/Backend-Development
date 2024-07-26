c32337c3a16e9e2e50ad048d74af47d85818aadc
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-07-26 06:32:21 +0530

    map completed

--- YoutubeBackend/script.js
+++ YoutubeBackend/script.js
@@ -21,6 +21,16 @@ var newarr = arr.map(function(val){
     return 13;
 })
 
-console.log(newarr);
+// console.log(newarr);
 // OUTPUT:
 // [13, 13, 13, 13]
+// var newarr = arr.map(function(val){
+//     return val;
+// })
+// O/P: [1, 2, 3, 4]
+var newarr = arr.map(function(val){
+    return val * 3;
+})
+// O/P: [3, 6, 9, 12] 
+console.log(newarr);
+
