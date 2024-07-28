16b93e4c5a22c7321ace684efd1aa8c471d07a11
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-07-28 23:29:16 +0530

    starting with setup

--- YoutubeBackend/Nodejs and npm-Part-2/script.js
+++ YoutubeBackend/Nodejs and npm-Part-2/script.js
@@ -1,6 +1,12 @@
 // Node.js Basics
 // Introduction to Node.js
 // Installing Node.js and npm
+
+// node and npm
+// working with node and npm
+// npm init
+// node basic setup
+
 // Working with modules
 
 // File System Operations
