0ea1bee6c1a71a7d86cf568fe72ff931067da37b
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-08-26 22:13:30 +0530

    intro to npm

---
+++ NPM Basics & Advanced Features/script.js
@@ -0,0 +1,13 @@
+// NPM understanding
+
+// npm - node package manager
+
+// NOTE:
+// node js core mein jo installed aata hai wo kehlata hain module
+// npm se download krte hai wo hota hai package
+
+// installing and uninstalling anything basics & advanced
+// understanding node_modules
+// dependencies
+// devdependencies
+// scripts - understanding default scripts PATH and custom scripts
