f2988a0d194cef2a68066cdff5842f989ebb2a1f
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-08-28 22:53:39 +0530

    topics gettig covered

---
+++ Express.js, Routing & Middleware Part 4/script.js
@@ -0,0 +1,11 @@
+// Express.js Framework:
+
+// Introduction to Express.js.
+// MERN STACK
+
+
+// Setting up a basic Express application
+// Routing
+// Middleware
+// Request and response handling
+// Error handling
