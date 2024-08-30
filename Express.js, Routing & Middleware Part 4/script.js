cae02ebbddff6b33ecf9d4f10fb12af3e48f428b
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-08-30 22:47:02 +0530

    Express and nodemon setup and basics of routing

--- Express.js, Routing & Middleware Part 4/script.js
+++ Express.js, Routing & Middleware Part 4/script.js
@@ -6,9 +6,19 @@
 // framework
 // manages everything from receiving the request and giving the response
 
-
 // Setting up a basic Express application
 // Routing
 // Middleware
 // Request and response handling
 // Error handling
+
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("champion meraaaaa anuuuuuuuuuj");
+});
+app.get("/profile", function (req, res) {
+  res.send("champion uska coach");
+});
+app.listen(3000);
