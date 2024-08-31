1654db7c9cc7044a64dcf694519fe89eedcc1598
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-08-31 19:48:15 +0530

    learnt about session and cookies

--- Form Handling, Sessions & Cookies Part-5/script.js
+++ Form Handling, Sessions & Cookies Part-5/script.js
@@ -1,3 +1,41 @@
 // form handling and working with the forms
 // handle backend process of forms and making sure the data coming from any frontend,
-// lig, fw, templating engines, we still handle it at the backend
+// lib, fw, templating engines, we still handle it at the backend
+
+// what is session and cookie?
+// cookie is something stored on frontend i.e in ur browser, that u get from server as when ur logged in
+// with a id like "abcd" attached to your request means 'req+abcd'
+
+// session is a time/thread where you are logged in and also with server, when this
+// session ends/logged out then new session begins
+
+const express = require('express')
+const app = express()
+
+// Session and Cookie
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+// Above 2 lines explanation
+// When data or information is transferred from browser to server
+// then at that time it doesn't give your data as plain text but as blob, which is a mixture of everything
+// tumne toh bheja tha plain text par server ko mila blob which is not directly readable ab is cheez ko handle
+// karna padega ki hum us blob ko waapas se readable kar sake
+
+app.get('/', function (req, res) {
+  res.send('champion mera anuj')
+})
+
+app.get('/about', function (req, res) {
+    res.send('about page hai yeh')
+  })
+
+  app.get('/profile', function (req, res, next) {
+    return next(new Error("Not Implemented"))
+  })
+
+  app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+  })
+
+app.listen(3000)
