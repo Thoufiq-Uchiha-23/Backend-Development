1ab42ef709e972334b3cd731afdb846ae5c691b7
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-08-31 18:26:11 +0530

    intro to form handling

---
+++ Form Handling, Sessions & Cookies Part-5/script.js
@@ -0,0 +1,3 @@
+// form handling and working with the forms
+// handle backend process of forms and making sure the data coming from any frontend,
+// lig, fw, templating engines, we still handle it at the backend
