53588b96d452b351a3826f9c8a879a562d1030e1
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-08-31 15:52:42 +0530

    basics of middleware is finished

--- Express.js, Routing & Middleware Part 4/script.js
+++ Express.js, Routing & Middleware Part 4/script.js
@@ -33,8 +33,27 @@
 const express = require("express");
 const app = express();
 
+// middleware initialized using "use()" method that has 3 fixed parameters
+// req, res, next
+app.use(function(req, res, next){
+  console.log("middleware chala");
+  // next method is used to forward the req otherwise it'll be stuck in loading
+  next();
+})
+
+// not just one we can make more than one middleware
+app.use(function(req, res, next){
+  console.log("middleware chala ek aur baar");
+  // next method is used to forward the req otherwise it'll be stuck in loading
+  next();
+})
+
 app.get("/", function (req, res) {
   res.send("champion meraaaaa anuuuuuuuuuj");
+});
+
+app.get("/about", function (req, res) {
+  res.send("about page hai yeh");
 });
 
 app.listen(3000);
